3f7a47cd54877456ab6ccfe55de27b4f59ee7d18
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-06-02 00:17:39 +0300

    first html draft update

--- movies/views.py
+++ movies/views.py
@@ -2,10 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Movie
 
-# Create your views here.
-
 
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([m.title for m in movies])
-    return HttpResponse(output)
+    return render(request, 'movies/index.html', {'movies': movies})
